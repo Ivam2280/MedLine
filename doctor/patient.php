a20850eed3c84263bd482a2798a69bb4653a123b
user <ivtovst@gmail.com>
2024-11-08 20:23:54 +0200

    Added functionality for searching and filtering patients

--- doctor/patient.php
+++ doctor/patient.php
@@ -89,6 +89,208 @@ $username=$userfetch["docname"];
                 
             </table>
         </div>
+        <?php       
 
+$selecttype="My";
+$current="My patients Only";
+if($_POST){
+
+    if(isset($_POST["search"])){
+        $keyword=$_POST["search12"];
+        
+        $sqlmain= "select * from patient where pemail='$keyword' or pname='$keyword' or pname like '$keyword%' or pname like '%$keyword' or pname like '%$keyword%' ";
+        $selecttype="my";
+    }
+    
+    if(isset($_POST["filter"])){
+        if($_POST["showonly"]=='all'){
+            $sqlmain= "select * from patient";
+            $selecttype="All";
+            $current="All patients";
+        }else{
+            $sqlmain= "select * from appointment inner join patient on patient.pid=appointment.pid inner join schedule on schedule.scheduleid=appointment.scheduleid where schedule.docid=$userid;";
+            $selecttype="My";
+            $current="My patients Only";
+        }
+    }
+}else{
+    $sqlmain= "select * from appointment inner join patient on patient.pid=appointment.pid inner join schedule on schedule.scheduleid=appointment.scheduleid where schedule.docid=$userid;";
+    $selecttype="My";
+}
+
+
+
+?>
+
+div class="dash-body">
+            <table border="0" width="100%" style=" border-spacing: 0;margin:0;padding:0;margin-top:25px; ">
+                <tr >
+                    <td>
+                        
+                        <form action="" method="post" class="header-search">
+
+                            <input type="search" name="search12" class="input-text header-searchbar" placeholder="Search Patient name or Email" list="patient">&nbsp;&nbsp;
+                            
+                            <?php
+                                echo '<datalist id="patient">';
+                                $list11 = $database->query($sqlmain);
+
+                                for ($y=0;$y<$list11->num_rows;$y++){
+                                    $row00=$list11->fetch_assoc();
+                                    $d=$row00["pname"];
+                                    $c=$row00["pemail"];
+                                    echo "<option value='$d'><br/>";
+                                    echo "<option value='$c'><br/>";
+                                };
+
+                            echo ' </datalist>';
+?>
+                            
+                       
+                            <input type="Submit" value="Search" name="search" class="login-btn btn-primary btn" style="padding-left: 25px;padding-right: 25px;padding-top: 10px;padding-bottom: 10px;">
+                        
+                        </form>
+                        
+                    </td>
+                    <td width="15%">
+                        <p style="font-size: 14px;color: rgb(119, 119, 119);padding: 0;margin: 0;text-align: right;">
+                            Today's Date
+                        </p>
+                        <p class="heading-sub12" style="padding: 0;margin: 0;">
+                            <?php 
+                        date_default_timezone_set('Europe/Kiev');
+                        $date = date('Y-m-d');
+                        echo $date;
+                        ?>
+                        </p>
+                    </td>
+                    <td width="10%">
+                        <button  class="btn-label"  style="display: flex;justify-content: center;align-items: center;"><img src="../img/calendar.svg" width="100%"></button>
+                    </td>
+
+
+                </tr>
+               
+                
+                <tr>
+                    <td colspan="4" style="padding-top:10px;">
+                        <p class="heading-main12" style="margin-left: 45px;font-size:18px;color:rgb(49, 49, 49)"><?php echo $selecttype." Patients (".$list11->num_rows.")"; ?></p>
+                    </td>
+                    
+                </tr>
+                
+                            </table>
+
+                        </center>
+                    </td>
+                    
+                </tr>
+                  
+                <tr>
+                   <td colspan="4">
+                       <center>
+                        <div class="abc scroll">
+                        <table width="93%" class="sub-table scrolldown"  style="border-spacing:0;">
+                        <thead>
+                        <tr>
+                                <th class="table-headin">
+                                    
+                                
+                                Name
+                                
+                                </th>
+
+                                <th class="table-headin">
+                                
+                            
+                                Telephone
+                                
+                                </th>
+                                <th class="table-headin">
+                                    Email
+                                </th>
+                                <th class="table-headin">
+                                    
+                                    Date of Birth
+                                    
+                                </th>
+                                <th class="table-headin">
+                                    
+                                    Events
+                                    
+                                </tr>
+                        </thead>
+                        <tbody>
+                        <?php
+
+                                
+$result= $database->query($sqlmain);
+if($result->num_rows==0){
+    echo '<tr>
+    <td colspan="4">
+    <br><br><br><br>
+    <center>
+    <img src="../img/notfound.svg" width="25%">
+    
+    <br>
+    <p class="heading-main12" style="margin-left: 45px;font-size:20px;color:rgb(49, 49, 49)">We  couldnt find anything related to your keywords !</p>
+
+    </center>
+    <br><br><br><br>
+    </td>
+    </tr>';
+    
+}
+else{
+for ( $x=0; $x<$result->num_rows;$x++){
+    $row=$result->fetch_assoc();
+    $pid=$row["pid"];
+    $name=$row["pname"];
+    $email=$row["pemail"];
+    $dob=$row["pdob"];
+    $tel=$row["ptel"];
+    
+    echo '<tr>
+        <td> &nbsp;'.
+        substr($name,0,35)
+        .'</td>
+
+        <td>
+            '.substr($tel,0,10).'
+        </td>
+        <td>
+        '.substr($email,0,20).'
+         </td>
+        <td>
+        '.substr($dob,0,10).'
+        </td>
+        <td >
+        <div style="display:flex;justify-content: center;">
+        
+        <a href="?action=view&id='.$pid.'" class="non-style-link"><button  class="btn-primary-soft btn button-icon btn-view"  style="padding-left: 40px;padding-top: 12px;padding-bottom: 12px;margin-top: 10px;"><font class="tn-in-text">View</font></button></a>
+       
+        </div>
+        </td>
+    </tr>';
+    
+}
+}
+ 
+?>
+
+</tbody>
+
+</table>
+</div>
+</center>
+</td> 
+</tr>
+
+
+
+</table>
+</div>
+</div>
+                        
 </body>
 </html>
