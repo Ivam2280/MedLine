690661c684abbc72d78820b76e55cc54b59997f6
Tymofii Kuzmenko <tymofii.kuzmenko@nure.ua>
2024-11-10 14:30:53 +0000

    Adding to doctor/schedule.php schedule display

--- doctor/schedule.php
+++ doctor/schedule.php
@@ -207,7 +207,37 @@ if($result->num_rows==0){
     
 }
 else {
-    <!----o------>
+    for ( $x=0; $x<$result->num_rows;$x++){
+        $row=$result->fetch_assoc();
+        $scheduleid=$row["scheduleid"];
+        $title=$row["title"];
+        $docname=$row["docname"];
+        $scheduledate=$row["scheduledate"];
+        $scheduletime=$row["scheduletime"];
+        $nop=$row["nop"];
+        echo '<tr>
+            <td> &nbsp;'.
+            substr($title,0,30)
+            .'</td>
+                                        
+            <td style="text-align:center;">
+                '.substr($scheduledate,0,10).' '.substr($scheduletime,0,5).'
+            </td>
+            <td style="text-align:center;">
+                '.$nop.'
+            </td>
+
+            <td>
+            <div style="display:flex;justify-content: center;">
+                                        
+            <a href="?action=view&id='.$scheduleid.'" class="non-style-link"><button  class="btn-primary-soft btn button-icon btn-view"  style="padding-left: 40px;padding-top: 12px;padding-bottom: 12px;margin-top: 10px;"><font class="tn-in-text">View</font></button></a>
+            &nbsp;&nbsp;&nbsp;
+            <a href="?action=drop&id='.$scheduleid.'&name='.$title.'" class="non-style-link"><button  class="btn-primary-soft btn button-icon btn-delete"  style="padding-left: 40px;padding-top: 12px;padding-bottom: 12px;margin-top: 10px;"><font class="tn-in-text">Cancel Session</font></button></a>
+            </div>
+            </td>
+           </tr>';
+                                    
+    }
 }
 }
                                  
