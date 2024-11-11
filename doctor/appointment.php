30b995a90e3c76a9b12f6a0e25023ad8fc06d300
Mordin228 <yehor.fatianov@nure.ua>
2024-11-12 00:13:08 +0100

    Add doctor details popup with session validation and user information display

--- doctor/appointment.php
+++ doctor/appointment.php
@@ -11,7 +11,20 @@
 </head>
 <body>
     <?php
-        //<!--k--->
+        session_start();
+
+        if(isset($_SESSION["user"])){
+            if(($_SESSION["user"])=="" or $_SESSION['usertype']!='d'){
+                header("location: ../index.php");
+            }else{
+                $useremail=$_SESSION["user"];
+            }
+    
+        }else{
+            header("location: ../index.php");
+        }
+        
+        include("../connection.php");
         $userrow = $database->query("select * from doctor where docemail='$useremail'");
         $userfetch=$userrow->fetch_assoc();
         $userid= $userfetch["docid"];
@@ -31,15 +44,162 @@
     ?>
     <!--p--->
     <?php
-        //<!--k--->
+        if($_GET){
+            $id=$_GET["id"];
+            $action=$_GET["action"];
+            if($action=='add-session'){
+    
+                echo '
+                <div id="popup1" class="overlay">
+                        <div class="popup">
+                        <center>
+                        
+                        
+                            <a class="close" href="schedule.php">&times;</a> 
+                            <div style="display: flex;justify-content: center;">
+                            <div class="abc">
+                            <table width="80%" class="sub-table scrolldown add-doc-form-container" border="0">
+                            <tr>
+                                    <td class="label-td" colspan="2">'.
+                                       ""
+                                    
+                                    .'</td>
+                                </tr>
+    
+                                <tr>
+                                    <td>
+                                        <p style="padding: 0;margin: 0;text-align: left;font-size: 25px;font-weight: 500;">Add New Session.</p><br>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td class="label-td" colspan="2">
+                                    <form action="add-session.php" method="POST" class="add-new-form">
+                                        <label for="title" class="form-label">Session Title : </label>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td class="label-td" colspan="2">
+                                        <input type="text" name="title" class="input-text" placeholder="Name of this Session" required><br>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    
+                                    <td class="label-td" colspan="2">
+                                        <label for="docid" class="form-label">Select Doctor: </label>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td class="label-td" colspan="2">
+                                        <select name="docid" id="" class="box" >
+                                        <option value="" disabled selected hidden>Choose Doctor Name from the list</option><br/>';
         $list11 = $database->query("select  * from  doctor;");
         for ($y=0;$y<$list11->num_rows;$y++){
             $row00=$list11->fetch_assoc();
             $sn=$row00["docname"];
             $id00=$row00["docid"];
-            //<!--k--->
+            echo "<option value=".$id00.">$sn</option><br/>";
         };
-        //<!--k--->
+        echo     '       </select><br><br>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td class="label-td" colspan="2">
+                                    <label for="nop" class="form-label">Number of Patients/Appointment Numbers : </label>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td class="label-td" colspan="2">
+                                    <input type="number" name="nop" class="input-text" min="0"  placeholder="The final appointment number for this session depends on this number" required><br>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td class="label-td" colspan="2">
+                                    <label for="date" class="form-label">Session Date: </label>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td class="label-td" colspan="2">
+                                    <input type="date" name="date" class="input-text" min="'.date('Y-m-d').'" required><br>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td class="label-td" colspan="2">
+                                    <label for="time" class="form-label">Schedule Time: </label>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td class="label-td" colspan="2">
+                                    <input type="time" name="time" class="input-text" placeholder="Time" required><br>
+                                </td>
+                            </tr>
+                           
+                            <tr>
+                                <td colspan="2">
+                                    <input type="reset" value="Reset" class="login-btn btn-primary-soft btn" >&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                                
+                                    <input type="submit" value="Place this Session" class="login-btn btn-primary btn" name="shedulesubmit">
+                                </td>
+                
+                            </tr>
+                           
+                            </form>
+                            </tr>
+                        </table>
+                        </div>
+                        </div>
+                    </center>
+                    <br><br>
+            </div>
+            </div>
+            ';
+        }elseif($action=='session-added'){
+            $titleget=$_GET["title"];
+            echo '
+            <div id="popup1" class="overlay">
+                    <div class="popup">
+                    <center>
+                    <br><br>
+                        <h2>Session Placed.</h2>
+                        <a class="close" href="schedule.php">&times;</a>
+                        <div class="content">
+                        '.substr($titleget,0,40).' was scheduled.<br><br>
+                            
+                        </div>
+                        <div style="display: flex;justify-content: center;">
+                        
+                        <a href="schedule.php" class="non-style-link"><button  class="btn-primary btn"  style="display: flex;justify-content: center;align-items: center;margin:10px;padding:10px;"><font class="tn-in-text">&nbsp;&nbsp;OK&nbsp;&nbsp;</font></button></a>
+                        <br><br><br><br>
+                        </div>
+                    </center>
+            </div>
+            </div>
+            ';
+        }elseif($action=='drop'){
+            $nameget=$_GET["name"];
+            $session=$_GET["session"];
+            $apponum=$_GET["apponum"];
+            echo '
+            <div id="popup1" class="overlay">
+                    <div class="popup">
+                    <center>
+                        <h2>Are you sure?</h2>
+                        <a class="close" href="appointment.php">&times;</a>
+                        <div class="content">
+                            You want to delete this record<br><br>
+                            Patient Name: &nbsp;<b>'.substr($nameget,0,40).'</b><br>
+                            Appointment number &nbsp; : <b>'.substr($apponum,0,40).'</b><br><br>
+                            
+                        </div>
+                        <div style="display: flex;justify-content: center;">
+                        <a href="delete-appointment.php?id='.$id.'" class="non-style-link"><button  class="btn-primary btn"  style="display: flex;justify-content: center;align-items: center;margin:10px;padding:10px;"<font class="tn-in-text">&nbsp;Yes&nbsp;</font></button></a>&nbsp;&nbsp;&nbsp;
+                        <a href="appointment.php" class="non-style-link"><button  class="btn-primary btn"  style="display: flex;justify-content: center;align-items: center;margin:10px;padding:10px;"><font class="tn-in-text">&nbsp;&nbsp;No&nbsp;&nbsp;</font></button></a>
+
+                        </div>
+                    </center>
+            </div>
+            </div>
+            '; 
+        }elseif($action=='view'){
         $sqlmain= "select * from doctor where docid='$id'";
         $result= $database->query($sqlmain);
         $row=$result->fetch_assoc();
@@ -51,7 +211,89 @@
         $spcil_array= $spcil_res->fetch_assoc();
         $spcil_name=$spcil_array["sname"];
         $tele=$row['doctel'];
-        //<!--k--->
+        echo '
+            <div id="popup1" class="overlay">
+                    <div class="popup">
+                    <center>
+                        <h2></h2>
+                        <a class="close" href="doctors.php">&times;</a>
+                        <div class="content">
+                            MedLine <br>
+                            
+                        </div>
+                        <div style="display: flex;justify-content: center;">
+                        <table width="80%" class="sub-table scrolldown add-doc-form-container" border="0">
+                        
+                            <tr>
+                                <td>
+                                    <p style="padding: 0;margin: 0;text-align: left;font-size: 25px;font-weight: 500;">View Details.</p><br><br>
+                                </td>
+                            </tr>
+                            
+                            <tr>
+                                
+                                <td class="label-td" colspan="2">
+                                    <label for="name" class="form-label">Name: </label>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td class="label-td" colspan="2">
+                                    '.$name.'<br><br>
+                                </td>
+                                
+                            </tr>
+                            <tr>
+                                <td class="label-td" colspan="2">
+                                    <label for="Email" class="form-label">Email: </label>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td class="label-td" colspan="2">
+                                '.$email.'<br><br>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td class="label-td" colspan="2">
+                                    <label for="Tele" class="form-label">Telephone: </label>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td class="label-td" colspan="2">
+                                '.$tele.'<br><br>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td class="label-td" colspan="2">
+                                    <label for="spec" class="form-label">Specialties: </label>
+                                    
+                                </td>
+                            </tr>
+                            <tr>
+                            <td class="label-td" colspan="2">
+                            '.$spcil_name.'<br><br>
+                            </td>
+                            </tr>
+                            <tr>
+                                <td colspan="2">
+                                    <a href="doctors.php"><input type="button" value="OK" class="login-btn btn-primary-soft btn" ></a>
+                                
+                                    
+                                </td>
+                
+                            </tr>
+                           
+
+                        </table>
+                        </div>
+                    </center>
+                    <br><br>
+            </div>
+            </div>
+            ';
+        }
+    }
+
     ?>
+    </div>
 </body>
 </html>
