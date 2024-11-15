a4bf3ae448bc69f3ca07a3cecfda20e5c6b4a6d3
user <ivtovst@gmail.com>
2024-11-15 22:26:58 +0200

    Implement doctor search results display with handling for no results found

--- patient/doctors.php
+++ patient/doctors.php
@@ -190,7 +190,61 @@ $username=$userfetch["pname"];
                         </thead>
                         <tbody>
                         
-                          
+                        <?php
+
+                                
+$result= $database->query($sqlmain);
+
+if($result->num_rows==0){
+    echo '<tr>
+    <td colspan="4">
+    <br><br><br><br>
+    <center>
+    <img src="../img/notfound.svg" width="25%">
+    
+    <br>
+    <p class="heading-main12" style="margin-left: 45px;font-size:20px;color:rgb(49, 49, 49)">We  couldnt find anything related to your keywords !</p>
+    </center>
+    <br><br><br><br>
+    </td>
+    </tr>';
+    
+}
+else{
+for ( $x=0; $x<$result->num_rows;$x++){
+    $row=$result->fetch_assoc();
+    $docid=$row["docid"];
+    $name=$row["docname"];
+    $email=$row["docemail"];
+    $spe=$row["specialties"];
+    $spcil_res= $database->query("select sname from specialties where id='$spe'");
+    $spcil_array= $spcil_res->fetch_assoc();
+    $spcil_name=$spcil_array["sname"];
+    echo '<tr>
+        <td> &nbsp;'.
+        substr($name,0,30)
+        .'</td>
+        <td>
+        '.substr($email,0,20).'
+        </td>
+        <td>
+            '.substr($spcil_name,0,20).'
+        </td>
+
+        <td>
+        <div style="display:flex;justify-content: center;">
+        
+        <a href="?action=view&id='.$docid.'" class="non-style-link"><button  class="btn-primary-soft btn button-icon btn-view"  style="padding-left: 40px;padding-top: 12px;padding-bottom: 12px;margin-top: 10px;"><font class="tn-in-text">View</font></button></a>
+       &nbsp;&nbsp;&nbsp;
+       <a href="?action=session&id='.$docid.'&name='.$name.'"  class="non-style-link"><button  class="btn-primary-soft btn button-icon menu-icon-session-active"  style="padding-left: 40px;padding-top: 12px;padding-bottom: 12px;margin-top: 10px;"><font class="tn-in-text">Sessions</font></button></a>
+        </div>
+        </td>
+    </tr>';
+    
+}
+}
+ 
+?>
  
                             </tbody>
 
