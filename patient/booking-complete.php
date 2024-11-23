d65ca9437941c5a233683c610140b1d9ee21d39f
Tymofii Kuzmenko <tymofii.kuzmenko@nure.ua>
2024-11-23 18:59:32 +0000

    Add sql queries for booking-complete.php to insert new bookings

--- patient/booking-complete.php
+++ patient/booking-complete.php
@@ -1,2 +1,15 @@
 <?php
+    //<!---k--->
+    $userrow = $database->query("select * from patient where pemail='$useremail'");
+    $userfetch=$userrow->fetch_assoc();
+    $userid= $userfetch["pid"];
+    $username=$userfetch["pname"];
+
+    if($_POST){
+        if(isset($_POST["booknow"])){
+            //<!---k--->
+            $sql2="insert into appointment(pid,apponum,scheduleid,appodate) values ($userid,$apponum,$scheduleid,'$date')";
+            //<!---k--->
+        }
+    }
 ?>
