8dc20194152819b0799c6610e31289a04b5d9612
Tymofii Kuzmenko <tymofii.kuzmenko@nure.ua>
2024-11-23 18:58:19 +0000

    Add sql queries to patient/appointments.php for retrieving appointment details from database

--- patient/appointment.php
+++ patient/appointment.php
@@ -10,6 +10,46 @@
     <title>Appointments</title>
 </head>
 <body>
+    <?php
+        //<!---k--->
+        $userrow = $database->query("select * from patient where pemail='$useremail'");
+        $userfetch=$userrow->fetch_assoc();
+        $userid= $userfetch["pid"];
+        $username=$userfetch["pname"];
+
+        $sqlmain= "select appointment.appoid,schedule.scheduleid,schedule.title,doctor.docname,patient.pname,schedule.scheduledate,schedule.scheduletime,appointment.apponum,appointment.appodate from schedule inner join appointment on schedule.scheduleid=appointment.scheduleid inner join patient on patient.pid=appointment.pid inner join doctor on schedule.docid=doctor.docid  where  patient.pid=$userid ";
+
+        if($_POST){
+            if(!empty($_POST["sheduledate"])){
+                $sheduledate=$_POST["sheduledate"];
+                $sqlmain.=" and schedule.scheduledate='$sheduledate' ";
+            }
+        }
+
+        $sqlmain.="order by appointment.appodate  asc";
+        $result= $database->query($sqlmain);
+    ?>
+    <!---p--->
+    <!---k--->
+    <!---p--->
+    <?php
+        if ($_GET) {
+            //<!---k--->
+        } elseif ($action=='view') {
+            $sqlmain= "select * from doctor where docid='$id'";
+            $result= $database->query($sqlmain);
+            $row=$result->fetch_assoc();
+            $name=$row["docname"];
+            $email=$row["docemail"];
+            $spe=$row["specialties"];
+            
+            $spcil_res= $database->query("select sname from specialties where id='$spe'");
+            $spcil_array= $spcil_res->fetch_assoc();
+            $spcil_name=$spcil_array["sname"];
+            $tele=$row['doctel'];
+            //<!---k--->
+        }
+    ?>
    
 </body>
 </html>
