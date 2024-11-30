0a8977e3315413ab0ff9d9cf92d8b285d1acfa63
user <ivtovst@gmail.com>
2024-11-30 22:13:36 +0200

    Develop a logic to filter schedules by date to manage  appointments and sessions

--- doctor/schedule.php
+++ doctor/schedule.php
@@ -4,39 +4,40 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/animations.css">  
     <link rel="stylesheet" href="../css/main.css">  
     <link rel="stylesheet" href="../css/admin.css">
         
     <title>Schedule</title>
+   
+</style>
 </head>
 <body>
+    <?php
 
-<?php
 
+    session_start();
 
-session_start();
+    if(isset($_SESSION["user"])){
+        if(($_SESSION["user"])=="" or $_SESSION['usertype']!='d'){
+            header("location: ../index.php");
+        }else{
+            $useremail=$_SESSION["user"];
+        }
 
-if(isset($_SESSION["user"])){
-    if(($_SESSION["user"])=="" or $_SESSION['usertype']!='d'){
-        header("location: ../index.php");
     }else{
-        $useremail=$_SESSION["user"];
+        header("location: ../index.php");
     }
+    
+    
 
-}else{
-    header("location: ../index.php");
-}
-
-
-
-include("../connection.php");
-$userrow = $database->query("select * from doctor where docemail='$useremail'");
-$userfetch=$userrow->fetch_assoc();
-$userid= $userfetch["docid"];
-$username=$userfetch["docname"];
-?>
-
-<div class="container">
+    include("../connection.php");
+    $userrow = $database->query("select * from doctor where docemail='$useremail'");
+    $userfetch=$userrow->fetch_assoc();
+    $userid= $userfetch["docid"];
+    $username=$userfetch["docname"];
+ ?>
+ <div class="container">
      <div class="menu">
      <table class="menu-container" border="0">
              <tr>
@@ -105,6 +106,7 @@ $username=$userfetch["docname"];
                             <?php 
 
                         date_default_timezone_set('Europe/Kiev');
+
                         $today = date('Y-m-d');
                         echo $today;
 
@@ -129,29 +131,52 @@ $username=$userfetch["docname"];
                     
                 </tr>
                 
-               
+                <tr>
+                    <td colspan="4" style="padding-top:0px;width: 100%;" >
+                        <center>
+                        <table class="filter-container" border="0" >
+                        <tr>
+                           <td width="10%">
+
+                           </td> 
+                        <td width="5%" style="text-align: center;">
+                        Date:
+                        </td>
+                        <td width="30%">
+                        <form action="" method="post">
+                            
+                            <input type="date" name="sheduledate" id="date" class="input-text filter-container-items" style="margin: 0;width: 95%;">
+
+                        </td>
+                        
+                    <td width="12%">
+                        <input type="submit"  name="filter" value=" Filter" class=" btn-primary-soft btn button-icon btn-filter"  style="padding: 15px; margin :0;width:100%">
+                        </form>
+                    </td>
+
+                    </tr>
                             </table>
 
                         </center>
                     </td>
                     
                 </tr>
-
+                
                 <?php
 
                 $sqlmain= "select schedule.scheduleid,schedule.title,doctor.docname,schedule.scheduledate,schedule.scheduletime,schedule.nop from schedule inner join doctor on schedule.docid=doctor.docid where doctor.docid=$userid ";
-    if($_POST){
-        $sqlpt1="";
-        if(!empty($_POST["sheduledate"])){
-            $sheduledate=$_POST["sheduledate"];
-            $sqlmain.=" and schedule.scheduledate='$sheduledate' ";
-        }
+                    if($_POST){
+                        $sqlpt1="";
+                        if(!empty($_POST["sheduledate"])){
+                            $sheduledate=$_POST["sheduledate"];
+                            $sqlmain.=" and schedule.scheduledate='$sheduledate' ";
+                        }
 
-    }
+                    }
 
-?>
-
-<tr>
+                ?>
+                  
+                <tr>
                    <td colspan="4">
                        <center>
                         <div class="abc scroll">
@@ -184,87 +209,86 @@ $username=$userfetch["docname"];
                                 </tr>
                         </thead>
                         <tbody>
-
-
-<?php
+                        
+                            <?php
 
                                 
-$result= $database->query($sqlmain);
+                                $result= $database->query($sqlmain);
 
-if($result->num_rows==0){
-    echo '<tr>
-    <td colspan="4">
-    <br><br><br><br>
-    <center>
-    <img src="../img/notfound.svg" width="25%">
-    
-    <br>
-    <p class="heading-main12" style="margin-left: 45px;font-size:20px;color:rgb(49, 49, 49)">We  couldnt find anything related to your keywords !</p>
-    </center>
-    <br><br><br><br>
-    </td>
-    </tr>';
-    
-}
-else {
-    for ( $x=0; $x<$result->num_rows;$x++){
-        $row=$result->fetch_assoc();
-        $scheduleid=$row["scheduleid"];
-        $title=$row["title"];
-        $docname=$row["docname"];
-        $scheduledate=$row["scheduledate"];
-        $scheduletime=$row["scheduletime"];
-        $nop=$row["nop"];
-        echo '<tr>
-            <td> &nbsp;'.
-            substr($title,0,30)
-            .'</td>
-                                        
-            <td style="text-align:center;">
-                '.substr($scheduledate,0,10).' '.substr($scheduletime,0,5).'
-            </td>
-            <td style="text-align:center;">
-                '.$nop.'
-            </td>
-
-            <td>
-            <div style="display:flex;justify-content: center;">
-                                        
-            <a href="?action=view&id='.$scheduleid.'" class="non-style-link"><button  class="btn-primary-soft btn button-icon btn-view"  style="padding-left: 40px;padding-top: 12px;padding-bottom: 12px;margin-top: 10px;"><font class="tn-in-text">View</font></button></a>
-            &nbsp;&nbsp;&nbsp;
-            <a href="?action=drop&id='.$scheduleid.'&name='.$title.'" class="non-style-link"><button  class="btn-primary-soft btn button-icon btn-delete"  style="padding-left: 40px;padding-top: 12px;padding-bottom: 12px;margin-top: 10px;"><font class="tn-in-text">Cancel Session</font></button></a>
-            </div>
-            </td>
-           </tr>';
+                                if($result->num_rows==0){
+                                    echo '<tr>
+                                    <td colspan="4">
+                                    <br><br><br><br>
+                                    <center>
+                                    <img src="../img/notfound.svg" width="25%">
                                     
-    }
-}
-}
+                                    <br>
+                                    <p class="heading-main12" style="margin-left: 45px;font-size:20px;color:rgb(49, 49, 49)">We  couldnt find anything related to your keywords !</p>
+                                    <a class="non-style-link" href="schedule.php"><button  class="login-btn btn-primary-soft btn"  style="display: flex;justify-content: center;align-items: center;margin-left:20px;">&nbsp; Show all Sessions &nbsp;</font></button>
+                                    </a>
+                                    </center>
+                                    <br><br><br><br>
+                                    </td>
+                                    </tr>';
+                                    
+                                }
+                                else{
+                                for ( $x=0; $x<$result->num_rows;$x++){
+                                    $row=$result->fetch_assoc();
+                                    $scheduleid=$row["scheduleid"];
+                                    $title=$row["title"];
+                                    $docname=$row["docname"];
+                                    $scheduledate=$row["scheduledate"];
+                                    $scheduletime=$row["scheduletime"];
+                                    $nop=$row["nop"];
+                                    echo '<tr>
+                                        <td> &nbsp;'.
+                                        substr($title,0,30)
+                                        .'</td>
+                                        
+                                        <td style="text-align:center;">
+                                            '.substr($scheduledate,0,10).' '.substr($scheduletime,0,5).'
+                                        </td>
+                                        <td style="text-align:center;">
+                                            '.$nop.'
+                                        </td>
+
+                                        <td>
+                                        <div style="display:flex;justify-content: center;">
+                                        
+                                        <a href="?action=view&id='.$scheduleid.'" class="non-style-link"><button  class="btn-primary-soft btn button-icon btn-view"  style="padding-left: 40px;padding-top: 12px;padding-bottom: 12px;margin-top: 10px;"><font class="tn-in-text">View</font></button></a>
+                                       &nbsp;&nbsp;&nbsp;
+                                       <a href="?action=drop&id='.$scheduleid.'&name='.$title.'" class="non-style-link"><button  class="btn-primary-soft btn button-icon btn-delete"  style="padding-left: 40px;padding-top: 12px;padding-bottom: 12px;margin-top: 10px;"><font class="tn-in-text">Cancel Session</font></button></a>
+                                        </div>
+                                        </td>
+                                    </tr>';
+                                    
+                                }
+                            }
                                  
-?>
+                            ?>
+ 
+                            </tbody>
 
-</tbody>
-
-</table>
-</div>
-</center>
-</td> 
-</tr>
-
-
-
-</table>
-</div>
-</div>
-
-<?php
-
-if($_GET){
-    $id=$_GET["id"];
-    $action=$_GET["action"];
-    if($action=='drop'){
-        $nameget=$_GET["name"];
-        echo '
+                        </table>
+                        </div>
+                        </center>
+                   </td> 
+                </tr>
+                       
+                        
+                        
+            </table>
+        </div>
+    </div>
+    <?php
+    
+    if($_GET){
+        $id=$_GET["id"];
+        $action=$_GET["action"];
+        if($action=='drop'){
+            $nameget=$_GET["name"];
+            echo '
             <div id="popup1" class="overlay">
                     <div class="popup">
                     <center>
@@ -283,10 +307,8 @@ if($_GET){
             </div>
             </div>
             '; 
-
-        
-    }elseif($action=='view'){
-        $sqlmain= "select schedule.scheduleid,schedule.title,doctor.docname,schedule.scheduledate,schedule.scheduletime,schedule.nop from schedule inner join doctor on schedule.docid=doctor.docid  where  schedule.scheduleid=$id";
+        }elseif($action=='view'){
+            $sqlmain= "select schedule.scheduleid,schedule.title,doctor.docname,schedule.scheduledate,schedule.scheduletime,schedule.nop from schedule inner join doctor on schedule.docid=doctor.docid  where  schedule.scheduleid=$id";
             $result= $database->query($sqlmain);
             $row=$result->fetch_assoc();
             $docname=$row["docname"];
@@ -342,7 +364,6 @@ if($_GET){
                                 '.$docname.'<br><br>
                                 </td>
                             </tr>
-                            
                             <tr>
                                 <td class="label-td" colspan="2">
                                 '.$scheduledate.'<br><br>
@@ -393,82 +414,82 @@ if($_GET){
                                  </thead>
                                  <tbody>';
                                  
-
-            
-            $result= $database->query($sqlmain12);
                 
-            if($result->num_rows==0){
-                echo '<tr>
-                <td colspan="7">
-                <br><br><br><br>
-                 <center>
-                 <img src="../img/notfound.svg" width="25%">
+                
+                                         
+                                         $result= $database->query($sqlmain12);
+                
+                                         if($result->num_rows==0){
+                                             echo '<tr>
+                                             <td colspan="7">
+                                             <br><br><br><br>
+                                             <center>
+                                             <img src="../img/notfound.svg" width="25%">
                                              
-                 <br>
-                 <p class="heading-main12" style="margin-left: 45px;font-size:20px;color:rgb(49, 49, 49)">We  couldnt find anything related to your keywords !</p>
-                 <a class="non-style-link" href="appointment.php"><button  class="login-btn btn-primary-soft btn"  style="display: flex;justify-content: center;align-items: center;margin-left:20px;">&nbsp; Show all Appointments &nbsp;</font></button>
-                 </a>
-                 </center>
-                 <br><br><br><br>
-                 </td>
-                 </tr>';
-
-            }
-            else{
-            for ( $x=0; $x<$result->num_rows;$x++){
-                $row=$result->fetch_assoc();
-                $apponum=$row["apponum"];
-                $pid=$row["pid"];
-                $pname=$row["pname"];
-                $ptel=$row["ptel"];
-                 echo '<tr style="text-align:center;">
-                 <td>
-                 '.substr($pid,0,15).'
-                 </td>
-                 <td style="font-weight:600;padding:25px">'.
+                                             <br>
+                                             <p class="heading-main12" style="margin-left: 45px;font-size:20px;color:rgb(49, 49, 49)">We  couldnt find anything related to your keywords !</p>
+                                             <a class="non-style-link" href="appointment.php"><button  class="login-btn btn-primary-soft btn"  style="display: flex;justify-content: center;align-items: center;margin-left:20px;">&nbsp; Show all Appointments &nbsp;</font></button>
+                                             </a>
+                                             </center>
+                                             <br><br><br><br>
+                                             </td>
+                                             </tr>';
+                                             
+                                         }
+                                         else{
+                                         for ( $x=0; $x<$result->num_rows;$x++){
+                                             $row=$result->fetch_assoc();
+                                             $apponum=$row["apponum"];
+                                             $pid=$row["pid"];
+                                             $pname=$row["pname"];
+                                             $ptel=$row["ptel"];
+                                             
+                                             echo '<tr style="text-align:center;">
+                                                <td>
+                                                '.substr($pid,0,15).'
+                                                </td>
+                                                 <td style="font-weight:600;padding:25px">'.
                                                  
-                 substr($pname,0,25)
-                 .'</td >
-                 <td style="text-align:center;font-size:23px;font-weight:500; color: var(--btnnicetext);">
-                 '.$apponum.'
+                                                 substr($pname,0,25)
+                                                 .'</td >
+                                                 <td style="text-align:center;font-size:23px;font-weight:500; color: var(--btnnicetext);">
+                                                 '.$apponum.'
                                                  
-                 </td>
-                 <td>
-                 '.substr($ptel,0,25).'
-                 </td>
+                                                 </td>
+                                                 <td>
+                                                 '.substr($ptel,0,25).'
+                                                 </td>
                                                  
                                                  
                 
                                                  
-                 </tr>';
+                                             </tr>';
+                                             
+                                         }
+                                     }
+                                          
+                                     
+                
+                                    echo '</tbody>
+                
+                                 </table>
+                                 </div>
+                                 </center>
+                            </td> 
+                         </tr>
 
-                 
-            }
-        }
-             
-        
+                        </table>
+                        </div>
+                    </center>
+                    <br><br>
+            </div>
+            </div>
+            ';  
+    }
+}
 
-       echo '</tbody>
-
-    </table>
+    ?>
     </div>
-    </center>
-</td> 
-</tr>
-
-</table>
-</div>
-</center>
-<br><br>
-</div>
-</div>
-';  
-}
-}
-
-?>
-</div>
-
 
 </body>
 </html>
